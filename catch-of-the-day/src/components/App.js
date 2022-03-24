851a1ef78e500bc25340038616fc452abccabbea
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-03-24 17:16:40 -0600

    Understanding state
    * pass fish as props to fish form

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,14 +5,26 @@ import Inventory from "./Inventory";
 
 
 class App extends React.Component{
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // 1. take copy of existing state
+    const fishes = {...this.state.fishes};
+  //  2. add new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+  //  3. set new fishes object to state
+    this.setState({ fishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
         </div>
-        <Inventory />
         <Order/>
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
