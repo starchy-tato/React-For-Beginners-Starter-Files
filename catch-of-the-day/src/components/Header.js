64aceabb3b2b1823ecf110789216826a41882ee6
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-03-24 09:15:56 -0600

    Update header component
    * from class to functional component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,20 +1,16 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>Catch
-          <span className="ofThe">
+const Header = props => (
+  <header className="top">
+    <h1>Catch
+      <span className="ofThe">
             <span className="of">Of</span>
             <span className="the">The</span>
           </span> Day</h1>
-        <h3 className="tagline">
-          <span>Fresh Seafood Market</span>
-        </h3>
-      </header>
-    )
-  }
-}
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+)
 
 export default Header;
